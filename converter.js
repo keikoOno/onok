7f761a07d8d160c0075923f47d595df9fed57b0c
keikoOno <90170754+keikoOno@users.noreply.github.com>
2025-06-03 15:03:43 +0900

    Update converter.js

--- converter.js
+++ converter.js
@@ -1,5 +1,6 @@
 (async()=>{
-  const u = "https://suumo.jp/jj/bukken/ichiran/JJ010FJ001/?ar=030&bs=011";
+const u = "https://suumo.jp/jj/bukken/ichiran/JJ010FJ001/?ar=030&bs=011";
   console.log("✅ SUUMOへジャンプ:", u);
-  open(u, '_blank');
+
+  return u;
 })();
